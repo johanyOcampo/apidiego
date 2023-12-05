cabf4fcfc1a07f3c89c7b431af1183db7ba66e46
johanyOcampo <joany246@hotmail.com>
2023-12-05 09:49:48 -0500

    apiparaconsumo2

---
+++ models/VentaSchema.js
@@ -0,0 +1,37 @@
+const VentaSchema = ({
+    nombreCliente: {
+        type: String,
+        unique: true,
+        required: [true, 'El nombre de usuario es requirido']
+    },
+
+    documento: {
+        type: String,
+        required: [true, 'El password es requeridod'],
+        min: [4, 'El password debe contener mínimo 4 caracteres'],
+        max: [10, 'El password debe contener máximo 10 caracteres']
+    },
+
+
+    correo: {
+        type: String,
+        required: [true, 'El password es requeridod'],
+    },
+
+    telefono: {
+        type: String,
+        required: [true, 'El password es requeridod'],
+    },
+
+    producto: {
+        type: String,
+        required: [true, 'El rol es requerido'],
+        enum: ['cuerdas', 'proteina', 'mancuernas'],
+    },
+
+    estado: {
+        type: Boolean,
+        default: true
+    }
+});
+exports.VentaSchema = VentaSchema;
